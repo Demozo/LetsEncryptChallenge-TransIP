66462a3807db42241bc20f4f052ddaa7ed735eed
Demozo <1168332+Demozo@users.noreply.github.com>
2022-09-01 18:17:45 +0200

    Wrap request body in dnsEntry block

--- src/DnsRecordUpdater.php
+++ src/DnsRecordUpdater.php
@@ -23,14 +23,17 @@ class DnsRecordUpdater
      * @throws Exception
      * @throws GuzzleException
      */
-    public function updateRecord(): bool {
+    public function updateRecord(): bool
+    {
         $accessToken = $this->createToken();
         $response = $this->httpClient->patch("domains/{$_SERVER['CERTBOT_DOMAIN']}/dns", [
             'body' => json_encode([
-                'name' => '_acme-challenge.',
-                'expire' => '60',
-                'type' => 'TXT',
-                'content' => $_SERVER['CERTBOT_TOKEN']
+                'dnsEntry' => [
+                    'name' => '_acme-challenge.',
+                    'expire' => '60',
+                    'type' => 'TXT',
+                    'content' => $_SERVER['CERTBOT_TOKEN']
+                ]
             ]),
             'headers' => [
                 'Content-Type' => "application/json",
@@ -44,7 +47,8 @@ class DnsRecordUpdater
     /**
      * @throws Exception
      */
-    private function createToken(): string {
+    private function createToken(): string
+    {
         $accessToken = new AccessToken();
 
         return $accessToken->createToken();
