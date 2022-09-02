1be10c2d0da9792d9ac8e1bed7651c7b7e660067
Demozo <1168332+Demozo@users.noreply.github.com>
2022-09-02 13:45:37 +0200

    Reverse TOKEN and VALIDATION in WellKnownUpdater

--- src/WellKnownUpdater.php
+++ src/WellKnownUpdater.php
@@ -9,8 +9,8 @@ class WellKnownUpdater
             mkdir('/var/www/html/.well-known/acme-challenge', 0774, true);
         }
 
-        $file = fopen("/var/www/html/.well-known/acme-challenge/{$_SERVER['CERTBOT_VALIDATION']}", 'w');
-        fwrite($file, $_SERVER['CERTBOT_TOKEN']);
+        $file = fopen("/var/www/html/.well-known/acme-challenge/{$_SERVER['CERTBOT_TOKEN']}", 'w');
+        fwrite($file, $_SERVER['CERTBOT_VALIDATION']);
         fclose($file);
 
         return true;
